c1a92a8daad4c34c7041580cd086dc468952c657
Assep Wahid <wahidassep5@gmail.com>
2025-09-02 20:08:27 +0700

    Add files via upload

---
+++ Assep_Project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.assep_project1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Assep_Project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Assep Wahid");
+        System.out.println("NPM : 2420506028");
+        System.out.println("Alamat : JL.Sanden ");
+        System.out.println("No_HP : 081922314423");
+    }
+}
