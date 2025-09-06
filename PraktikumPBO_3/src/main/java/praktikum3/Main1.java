28bdcf93ebccc9275eaaab229b3dc57ee19799c7
Assep Wahid <wahidassep5@gmail.com>
2025-09-07 02:18:31 +0700

    Add files via upload

---
+++ PraktikumPBO_3/src/main/java/praktikum3/Main1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum3;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Main1 {
+    public static void main(String[] args) {
+        Mobil mobil1 = new Mobil("Toyota", "Avanza", 2020, "Hitam");
+        Mobil mobil2 = new Mobil("Honda", "Civic", 2022, "Putih");
+
+        mobil1.displayInfo();
+        mobil1.startEngine();
+
+        System.out.println();
+
+        mobil2.displayInfo();
+        mobil2.startEngine();
+
+        mobil1.ubahWarna("Merah");
+        mobil1.displayInfo();
+    }
+}
